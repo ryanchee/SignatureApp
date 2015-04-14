aef6d40bcd3bcba18090c43c5f4710e5ebf42365
Ronald Rosales <yayadrevis@Ronalds-Mac.local>
2015-04-13 23:38:10 -0700

    Login working, we believe

--- SignatureApp/ViewController.swift
+++ SignatureApp/ViewController.swift
@@ -10,6 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var usernameField : UITextField!
+    @IBOutlet var passField : UITextField!
+    @IBAction func loginButton() {
+        
+        PFUser.logInWithUsernameInBackground(usernameField.text, password:passField.text) {
+            (user: PFUser!, error: NSError!) -> Void in
+            if user != nil {
+                // Do stuff after successful login.
+                println("\(self.usernameField.text)")
+                self.performSegueWithIdentifier("segueTest", sender: self)
+            } else {
+                // The login failed. Check error to see why.
+                println("nan")
+            }
+        }
+    }
+    
+    override func shouldPerformSegueWithIdentifier(identifier: String!, sender: AnyObject!) -> Bool {
+        if identifier == "segueTest" {
+            
+            if (self.usernameField.text.isEmpty) {
+                
+                let alert = UIAlertView()
+                alert.title = "No Text"
+                alert.message = "Please Enter Text In The Box"
+                alert.addButtonWithTitle("Ok")
+                alert.show()
+                
+                return false
+            }
+            
+            else {
+                
+               return true
+           }
+        }
+        
+        // by default, transition
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +60,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 
 }
 
